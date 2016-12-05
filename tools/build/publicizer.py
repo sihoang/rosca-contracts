2a66e5f7036b5bafc74e6247590105fc7d43e56d
Ron Merom <rmerom@gmail.com>
2016-12-05 08:57:07 +0200

    backup original file and overwrite it with modified version

--- tools/build/publicizer.py
+++ tools/build/publicizer.py
@@ -3,20 +3,24 @@
 """
 Usage: publicizer.py someFile.sol
 
-Replaces
+Replaces both
   uint256 internal /* publicForTesting */ someVar;
+  and
+  uint256 private /* publicForTesting */ someVar;
 with
   uint256 public /* modifiedForTest */ someVar;
 
-Also replaces "private" instead of "internal", as long as they're followed by
-/* publicForTesting */
-
-The modified file is printed to stdout.
+NOTE: The original file f is backed up to f.backup.<current time> and is overwritten
+with the modified file.
 
 """
 
+import math
 import re
+from shutil import copyfile
 import sys
+import time
+
 
 
 def substitute(content):
@@ -25,9 +29,14 @@ def substitute(content):
   p = re.compile(r'\s+(internal|public)\s*/\*\s*publicForTesting\s*\*/\s*')
   replaceWith = ' public /* modifiedForTest */ '
 
-  return p.sub(replaceWith, content)
+  replacedContent =  p.sub(replaceWith, content)
+  return replacedContent
 
 if __name__ == "__main__":
-  with open(sys.argv[1]) as file:
-    print(substitute(file.read()))
+  currentTime = math.floor(time.time())
+  copyfile(sys.argv[1], sys.argv[1] + ".backup." + str(currentTime))
+  with open(sys.argv[1], "r+") as file:
+    newcontent = substitute(file.read())
+    file.seek(0)
+    file.write(newcontent)
 
